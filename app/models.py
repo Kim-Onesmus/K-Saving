120bf5ba6bf072be20770a3a0b28d0f36da583e0
Kimanzi <kimones1122@gmail.com>
2023-07-12 04:57:45 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(null=True, blank=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
