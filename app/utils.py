a0c8fff32232aa551b539f4aef18a79a3e38dbe2
Kimanzi <kimones1122@gmail.com>
2023-09-21 06:25:21 +0300

    changes

--- app/utils.py
+++ app/utils.py
@@ -2,15 +2,15 @@ from PIL import Image, ImageDraw, ImageFont
 from django.conf import settings
 import os
 
-def generate_default_profile_picture(user, font_size=36):
+def generate_default_profile_picture(user, font_size=72):
     initials = user.first_name[0] + user.last_name[0]
     size = (100, 100)  # Set the desired size of the image
     image = Image.new('RGB', size, 'green')
     draw = ImageDraw.Draw(image)
     
-    # Specify the font size directly in the code
-    font = ImageFont.load_default()
-    font = font.font_variant(size=font_size)
+    # Load the TTF font file and specify the font size
+    font_path = os.path.join(settings.STATIC_ROOT, 'static/css/AGAALER.TTF')
+    font = ImageFont.truetype(font_path, font_size)
     
     text_size = draw.textsize(initials, font=font)
     position = ((size[0] - text_size[0]) / 2, (size[1] - text_size[1]) / 2)
@@ -24,3 +24,5 @@ def generate_default_profile_picture(user, font_size=36):
     
     # Return the relative URL of the saved image
     return os.path.join('media', filename)
+
+    print(f"Text Size: {text_size}")
