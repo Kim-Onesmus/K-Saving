3a42dec025aef3469d4a808cc91792015fc21bad
Kimanzi <kimones1122@gmail.com>
2023-09-22 07:43:00 +0300

    changes

--- app/models.py
+++ app/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from .utils import generate_default_profile_picture
 # Create your models here.
 
+def user_profile_picture_path(instance, filename):
+    # Generate a unique filename for the profile picture
+    ext = filename.split('.')[-1]
+    filename = f'media/{instance.user.username}_profile_pic.{ext}'
+    return os.path.join(settings.MEDIA_ROOT, filename)
+
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(null=True, blank=True, upload_to='media')
@@ -15,6 +21,11 @@ class Client(models.Model):
         if not self.profile_picture:
             self.profile_picture.name = generate_default_profile_picture(self.user)
         super().save(*args, **kwargs)
+        
+    def update_profile_picture(self, image):
+        # Update the profile picture
+        self.profile_picture = image
+        self.save()
         
         
     def __str__(self):
