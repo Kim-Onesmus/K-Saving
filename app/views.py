2a107fbe5112716fd1500784079c1ec7e2336149
Kimanzi <kimones1122@gmail.com>
2023-09-27 13:15:48 +0300

    changes

--- app/views.py
+++ app/views.py
@@ -201,7 +201,7 @@ def Withdraw(request):
     return render(request, 'app/transaction/withdraw.html')
 
 def Deposits(request):
-    user = request.client.user
+    # user = MpesaPayment.objects.filter(id=pk)
     return render(request, 'app/history/deposits.html')
 
 def Withdrawals(request):
