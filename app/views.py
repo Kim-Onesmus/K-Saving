af24e591933d0e16628b8e46b97b101c2316bcfc
Kim-Onesmus <kimones1122@gmail.com>
2023-10-25 15:00:40 +0300

    changes

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ def Index(request):
 def MyPlan(request):
     user = request.user
     client = user.client
-    existing_plan = My_Plan.objects.filter(client=client)
+    existing_plan = My_Plan.objects.filter(client=client).first()
     form = My_PlanForm(instance=existing_plan)
     
     if request.method == 'POST':
