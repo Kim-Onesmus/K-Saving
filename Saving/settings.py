094c361f20e75dd03308a8b1385f79d1248ff38b
Kim-Onesmus <kimones1122@gmail.com>
2024-01-26 11:24:22 +0300

    changes

--- Saving/settings.py
+++ Saving/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v37i%tt#3g_j_xy9z03+jz%ay#j936flo&pg@z3(f!dk9qd)ya
 DEBUG = True
 
 # CSRF_TRUSTED_ORIGINS = ['https://eed6-105-160-100-159.ngrok-free.app'],
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
