61214e3342cfd653e28f6a93dc1e55e9719caa7a
Kim-Onesmus <kimones1122@gmail.com>
2024-02-07 13:23:18 +0300

    changes

--- app/tests.py
+++ app/tests.py
@@ -3,13 +3,3 @@ from django.test import TestCase
 # Create your tests here.
 
 
-          <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-            <i id="bellCount" class="bi bi-bell" data-count="0"></i>
-          </a>
-
-
-                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-            <i id="bellCount" class="fa-solid fa-bell" data-count="0"></i>
-            <i class="bi bi-bell"></i>
-            <span class="badge bg-primary badge-number">4</span>
-          </a>
