8bce33de81e2bd35dbbf115b4cabebf9257b6b51
Kim-Onesmus <kimones1122@gmail.com>
2024-11-05 12:22:46 +0300

    updates

--- Saving/settings.py
+++ Saving/settings.py
@@ -134,8 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIR = os.path.join(BASE_DIR, 'app/static')
+
 
 
 MEDIA_URL = 'media/'
